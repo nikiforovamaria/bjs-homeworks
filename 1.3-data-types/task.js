14fff892b6cb6eca9be95d47db66e6683c955326
Maria Nikiforova <nikiforovamary@gmail.com>
2020-10-08 20:39:41 +0600

    second commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -4,27 +4,26 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let parsedContribution = parseInt(contribution);
     let parsedAmount = parseInt(amount);
     if (isNaN(parsedPercent)) {
-        console.log(percent);
         return `Параметр процентная ставка содержит неправильное значение ${percent}`;
     } else if (isNaN(parsedContribution)) {
         return `Параметр сумма первоначального взноса содержит неправильное значение ${contribution}`;
     } else if (isNaN(parsedAmount)) {
         return `Параметр сумма кредита содержит неправильное значение ${amount}`;
     }
-    let remainingAmount = parsedAmount - parsedContribution
-    let months = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
+    let remainingAmount = parsedAmount - parsedContribution;
+    const currentDate = new Date();
+    let months = date.getMonth() - currentDate.getMonth() + (12 * (date.getFullYear() - currentDate.getFullYear()));
     let finalPercent = (parsedPercent / 100) / 12;
     let monthlyPayment = remainingAmount * (finalPercent + finalPercent / (((1 + finalPercent)**months) - 1));
     let totalAmount = monthlyPayment * months;
+    totalAmount = new Number(totalAmount.toFixed(2));
 
-    console.log(totalAmount.toFixed(2));
-    return totalAmount.toFixed(2);
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    if (!name) {
-        return 'Привет, мир! Меня зовут Аноним.';
-    } else {
-        return `Привет, мир! Меня зовут ${name}`;
-    }
+    let greeting = `Привет, мир! Меня зовут ${!name || name === 'null' || name === '' || name === 'undefined' ? 'Аноним' : name}`;
+    
+    return greeting;
 }
