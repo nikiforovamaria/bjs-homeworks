0f9edc4b638bcc25e95a4889cbead9752994a1e3
Maria Nikiforova <nikiforovamary@gmail.com>
2020-10-08 13:05:27 +0600

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,30 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+    let parsedPercent = parseInt(percent); 
+    let parsedContribution = parseInt(contribution);
+    let parsedAmount = parseInt(amount);
+    if (isNaN(parsedPercent)) {
+        console.log(percent);
+        return `Параметр процентная ставка содержит неправильное значение ${percent}`;
+    } else if (isNaN(parsedContribution)) {
+        return `Параметр сумма первоначального взноса содержит неправильное значение ${contribution}`;
+    } else if (isNaN(parsedAmount)) {
+        return `Параметр сумма кредита содержит неправильное значение ${amount}`;
+    }
+    let remainingAmount = parsedAmount - parsedContribution
+    let months = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
+    let finalPercent = (parsedPercent / 100) / 12;
+    let monthlyPayment = remainingAmount * (finalPercent + finalPercent / (((1 + finalPercent)**months) - 1));
+    let totalAmount = monthlyPayment * months;
+
+    console.log(totalAmount.toFixed(2));
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (!name) {
+        return 'Привет, мир! Меня зовут Аноним.';
+    } else {
+        return `Привет, мир! Меня зовут ${name}`;
+    }
 }
