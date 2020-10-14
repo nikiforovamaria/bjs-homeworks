991f1ee5da136e9347844b67bc8502fca6ea62ff
Maria Nikiforova <nikiforovamary@gmail.com>
2020-10-14 21:58:44 +0600

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,27 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    let str = this.toLowerCase();
+    str = str.split(' ').join('');
+    return str === str.split("").reverse().join("") ? true : false;
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let average = 0;
+    if (marks === 0) {
+        return average;
+    }
+    for (let i = 0; i < marks.length; i++) {
+        average += marks[i]; 
+    }
+    let roundedAverage = Math.round(average / marks.length);
+
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = +Date.now();
+    let bDay = +new Date(birthday);
+    let diff = now - bDay;
+    let age = Math.floor(diff / 1000 / 60 / 60 / 24 / 365.25);
+
+    return age;
 }
