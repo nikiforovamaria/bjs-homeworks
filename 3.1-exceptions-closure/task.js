033fa65cce4593d952b34dd5a83a71e742da9dc3
Maria Nikiforova <nikiforovamary@gmail.com>
2020-10-23 12:52:06 +0600

    second commit array-proto-hocs

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -19,37 +19,34 @@ class Triangle {
         this.a = a;
         this.b = b;
         this.c = c;
-    }
-    checkTriangle() {
-        if ((this.a + this.b < this.c) || (this.b + this.c < this.a) || (this.c + this.a < this.b)) {
+        try {
+            if ((this.a + this.b < this.c) || (this.b + this.c < this.a) || (this.c + this.a < this.b)) {
             throw new Error('Треугольник с такими сторонами не существует');
-        } else {
-            return new Triangle(this.a, this.b, this.c);
+            }
+        } catch(e) {
+            return e;
         }
     }
     getPerimeter() {
-        if (this.checkTriangle() == new Error) {
-            throw new Error('Ошибка! Треугольник не существует');
-        } else {
         const P = this.a + this.b + this.c;
         return P;
-        }
     }
     getArea() {
         const p = this.getPerimeter() / 2;
         const S = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-        return S.toFixed(3);
+        return parseFloat(S.toFixed(3));
     }
 }
 
 function getTriangle(a, b, c) {
     const tri = new Triangle(a, b, c);
     try {
-        tri.checkTriangle();
-    } catch(e) {
-        return e;
-    } finally {
-        tri.getArea();
-        tri.getPerimeter();
+        return tri;
+    } catch {
+        tri = {
+            getPerimeter: () => 'Ошибка! Треугольник не существует',
+            getArea: () => 'Ошибка! Треугольник не существует'
+        }
+        return tri;
     }
 }
